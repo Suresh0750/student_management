51a46cddf847d2f0eefb98d57ae7ab80398ebff1
Suresh0750 <sureshvarma0750@gmail.com>
2026-04-18 12:07:25 +0530

    feat : setup database

---
+++ server/src/config/database.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+
+  const dbConnect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI!);
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.log(err);
+    }
+  }

a594b1148785fb6c332a51e48e1680c361dd675a
Suresh0750 <sureshvarma0750@gmail.com>
2026-04-18 12:59:05 +0530

    feat : JWT Token done

---
+++ server/src/utils/jwt.ts
@@ -0,0 +1,11 @@
+import jwt from "jsonwebtoken";
+import { IUsers, TokenPayload } from "../interface/users";
+
+
+export const generateToken = ({ userId, role, name, email }: TokenPayload) => {
+  return jwt.sign(
+    { userId, role, name, email },
+    process.env.JWT_SECRET!,
+    { expiresIn: "1h" }
+  );
+};
